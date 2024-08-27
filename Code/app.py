180cce835c27583032f3be3f6524c7ee7ffcb9ef
Yashwanth Pindi <63833299+ypindi@users.noreply.github.com>
2024-08-27 11:43:58 +0530

    feat: Style of Writing and Citations Generator
    Added 2 new features:
    1. Style of Essay Generation: Users can select the writing style (e.g., formal, informal, persuasive), which is available as a dropdown option.
    2. Citations Generator: generates that automatically generates citations for the research used in the essays.

--- Code/app.py
+++ Code/app.py
@@ -13,6 +13,7 @@ os.environ['GOOGLE_API_KEY'] = apikey
 
 st.title('Essay Creator using Google Gemini API Key')
 prompt = st.text_input('Please add your prompt here!')
+style = st.selectbox('Choose the writing style:', ['Formal', 'Informal', 'Persuasive'])
 
 title_template = PromptTemplate(
     input_variables = ['topic'], 
@@ -21,8 +22,14 @@ title_template = PromptTemplate(
 
 script_template = PromptTemplate(
     input_variables = ['title', 'wikipedia_research'], 
-    template='write me an essay based on this title: {title} using Wikipedia Research:{wikipedia_research} '
+    template='write me an essay based on this title: {title} using Wikipedia Research:{wikipedia_research} in a {style} style.'
 )
+
+citation_template = PromptTemplate(
+    input_variables=['title', 'wikipedia_citation'],
+    template='Generate a citation list for the following sources used in an essay titled {title}. Wikipedia: {wikipedia_citation}.'
+)
+
 
 title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
 script_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
@@ -35,12 +42,17 @@ llm = GoogleGenerativeAI(
 
 title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
 script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script', memory=script_memory)
+citation_chain = LLMChain(llm=llm, prompt=citation_template, verbose=True, output_key='citations')
 wiki = WikipediaAPIWrapper()
 
 if prompt: 
     title = title_chain.run(prompt)
     wiki_research = wiki.run(prompt) 
-    script = script_chain.run(title=title, wikipedia_research=wiki_research)
+    script = script_chain.run(title=title, wikipedia_research=wiki_research, style = style)
+    citations = citation_chain.run(
+        title=title, 
+        wikipedia_citation=f"Wikipedia: {wiki_research[:50]}... (Retrieved from Wikipedia)"
+    )
 
     st.write(title) 
     st.write(script) 
@@ -53,4 +65,7 @@ if prompt:
 
     with st.expander('Wikipedia Research'): 
         st.info(wiki_research)
+
+    with st.expander('Citations'): 
+        st.write(citations)
 
